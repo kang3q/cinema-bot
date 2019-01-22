d5b229489fd0bb45ce8b483efc6f70ddea4aaa2b
1004w455 <1004w455@naver.com>
2019-01-22 22:05:11 +0900

    feat: megabox 초기값 설정

--- src/main/java/com/bot/cinemabot/scheduler/MegaboxScheduler.java
+++ src/main/java/com/bot/cinemabot/scheduler/MegaboxScheduler.java
@@ -1,11 +1,13 @@
 package com.bot.cinemabot.scheduler;
 
+import com.bot.cinemabot.Telegram;
 import com.bot.cinemabot.model.megabox.MegaboxResponse;
 import com.bot.cinemabot.model.megabox.MegaboxTicket;
 import com.bot.cinemabot.utils.Utils;
 import lombok.extern.slf4j.Slf4j;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,24 +16,32 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 //@Component
 @RestController
 public class MegaboxScheduler {
 
-//	@Autowired
-//	private Telegram telegram;
+	@Autowired
+	private Telegram telegram;
 
 	@Value("${bot.cinema.megabox.api}")
 	private String megabox;
 	@Value("${bot.cinema.megabox.detailUrl}")
 	private String detailUrl;
 
+	private List<MegaboxTicket> cache1p1Tickets;
+
 	@PostConstruct
 	public void init() throws IOException {
-		getMegaboxTickets();
+		List<MegaboxTicket> allTickets =  getMegaboxTickets();
+		List<MegaboxTicket> onePlusOneTickets = filtered1p1Tickets(allTickets);
+		cache1p1Tickets = Collections.synchronizedList(onePlusOneTickets);
+		telegram.sendMessageToBot("메가박스\n모든 관람권: %s\n1+1 관람권: %s",
+				allTickets.size(), onePlusOneTickets.size());
 	}
 
 //	@Scheduled(initialDelay = 1_000, fixedDelayString = "5000")
@@ -59,6 +69,12 @@ public class MegaboxScheduler {
 		String price = megaboxDocument.select("#content #displayPrice").val();
 		String name = megaboxDocument.select("#form_pay input[name=itemName]").val();
 		return new MegaboxTicket(name, price, date, detailUrl + itemCode, false);
+	}
+
+	private List<MegaboxTicket> filtered1p1Tickets(List<MegaboxTicket> tickets) {
+		return tickets.stream()
+				.filter(t -> t.getName().contains("1+1"))
+				.collect(Collectors.toList());
 	}
 
 }
