a6dcac16cb880a3589af92fc3305b777761c9831
1004w455 <1004w455@naver.com>
2019-02-07 12:38:24 +0900

    chore: url encode 적용

--- src/main/java/com/bot/cinemabot/utils/Telegram.java
+++ src/main/java/com/bot/cinemabot/utils/Telegram.java
@@ -14,6 +14,9 @@ import com.bot.cinemabot.utils.Utils;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
 @Slf4j
 @Component
 public class Telegram {
@@ -46,11 +49,15 @@ public class Telegram {
     public void sendMessageToBot(String message, Object... obj) {
         message = String.format(message, obj);
         log.info(message);
-        String telegramSendMessageUrl = sendMessageUrl + String.format("?chat_id=%s&text=%s", chatId, message);
-        String response = Utils.restTemplate.getForObject(telegramSendMessageUrl, String.class);
-//        template.convertAndSend("/topic/greetings", new Greeting(message));
-        if (!response.contains("\"ok\":true")) {
-            log.error("텔레그램 메시지 전송 실패. {}", response);
+        try {
+            String telegramSendMessageUrl = sendMessageUrl + String.format("?chat_id=%s&text=%s", chatId, URLEncoder.encode(message, "UTF-8"));
+            String response = Utils.restTemplate.getForObject(telegramSendMessageUrl, String.class);
+//            template.convertAndSend("/topic/greetings", new Greeting(message));
+            if (!response.contains("\"ok\":true")) {
+                log.error("텔레그램 메시지 전송 실패. {}", response);
+            }
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
         }
     }
 
