1b79b70a64d5335a0c441793899e79b4e58640cd
1004w455 <1004w455@naver.com>
2019-07-29 11:10:24 +0900

    chore: run/update 실행 시 절대경로 적용 (빠진부분 추가)

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 sh /home/pi/cinema-bot/stop.sh
 
 echo 'start cinema-bot'
-nohup java -jar target/cinema-bot-0.0.1-SNAPSHOT.jar --bot.telegram.token=$CINEMA_BOT_TOKEN &
+nohup java -jar /home/pi/cinema-bot/target/cinema-bot-0.0.1-SNAPSHOT.jar --bot.telegram.token=$CINEMA_BOT_TOKEN &
 
 echo 'END run.sh'
