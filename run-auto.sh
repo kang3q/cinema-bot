da7324dfc4393aafaa9ae14e9650ce4bc5fc786d
1004w455 <1004w455@naver.com>
2019-07-29 14:00:22 +0900

    chore: nohup.out 로그파일 생성 안되도록

--- run-auto.sh
+++ run-auto.sh
@@ -1,1 +1,1 @@
-nohup java -jar /home/pi/cinema-bot/target/cinema-bot-0.0.1-SNAPSHOT.jar --bot.telegram.token=$CINEMA_BOT_TOKEN > /dev/null &
+nohup java -jar /home/pi/cinema-bot/target/cinema-bot-0.0.1-SNAPSHOT.jar --bot.telegram.token=$CINEMA_BOT_TOKEN 1> /dev/null 2>&1 &
