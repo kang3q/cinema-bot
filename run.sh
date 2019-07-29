b598a3d925945c90c3185468625c418aec22b068
1004w455 <1004w455@naver.com>
2019-07-29 11:40:24 +0900

    chore: run/update 실행 시 절대경로 적용 (test 정상실행이 안되고 있다)

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
-sh /home/pi/cinema-bot/stop.sh
+sudo sh /home/pi/cinema-bot/stop.sh
 
 echo 'start cinema-bot'
-nohup java -jar /home/pi/cinema-bot/target/cinema-bot-0.0.1-SNAPSHOT.jar --bot.telegram.token=$CINEMA_BOT_TOKEN &
+sudo nohup java -jar /home/pi/cinema-bot/target/cinema-bot-0.0.1-SNAPSHOT.jar --bot.telegram.token=$CINEMA_BOT_TOKEN &
 
 echo 'END run.sh'
--- stop.sh
+++ stop.sh
@@ -1,4 +1,4 @@
 echo 'pkill cinema-bot'
-pkill -9 -ef cinema-bot
+sudo pkill -9 -ef cinema-bot
 
 echo 'END stop.sh'
