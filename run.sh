a619d3845fbe39883eab3151830d9b68084e41ef
1004w455 <1004w455@naver.com>
2019-08-05 19:12:34 +0900

    chore: 로그는 필요하다

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 sh stop.sh
 
 echo 'start cinema-bot'
-nohup java -jar target/cinema-bot-0.0.1-SNAPSHOT.jar --bot.telegram.token=$CINEMA_BOT_TOKEN 1> /dev/null 2>&1 &
+nohup java -jar target/cinema-bot-0.0.1-SNAPSHOT.jar --bot.telegram.token=$CINEMA_BOT_TOKEN
 
 echo 'END run.sh'
