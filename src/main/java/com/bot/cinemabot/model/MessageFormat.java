a803a89c4cbba93598c670e2f4f0ce2dc45c692e
1004w455 <1004w455@naver.com>
2019-08-14 11:16:48 +0900

    fix: html 템플릿 오류 수정

--- src/main/java/com/bot/cinemabot/model/MessageFormat.java
+++ src/main/java/com/bot/cinemabot/model/MessageFormat.java
@@ -18,13 +18,13 @@ public class MessageFormat {
 	}
 
 	public String convertHTML() {
-		return String.format(getHTMLTemplate(), this.platform, this.title, this.dateRange, this.price, this.exist1p1, this.existTicket, this.buyUrl);
+		return String.format(getHTMLTemplate(), this.platform, this.title, this.dateRange, this.price, this.exist1p1, this.existTicket, this.buyUrl, this.buyUrl);
 	}
 
 	private String getTextTemplate() {
 		switch (this.platform) {
 			case "CGV":
-				return "%s\n%s\n오후 2시 판매시작!(or 4시)\n%s%s\n1+1영화:%s종\n구매링크:%s";
+				return "%s\n%s\n오후 2시 판매시작!(or 4시)\n%s%s\n1+1영화:%s종%s\n구매링크:%s";
 			case "롯데시네마":
 				return "%s\n%s\n%s\n%s원\n1+1영화:%s종, 티켓:%s종\n구매링크:%s";
 			case "메가박스":
@@ -36,7 +36,7 @@ public class MessageFormat {
 	private String getHTMLTemplate() {
 		switch (this.platform) {
 			case "CGV":
-				return "%s\n<b>%s</b>\n오후 2시 판매시작!(or 4시)\n%s%s\n1+1영화: %s종\n<a href=\"%s\">%s</a>";
+				return "%s\n<b>%s</b>\n오후 2시 판매시작!(or 4시)\n%s%s\n1+1영화: %s종%s\n<a href=\"%s\">%s</a>";
 			case "롯데시네마":
 				return "%s\n<b>%s</b>\n%s\n%s원\n1+1영화: %s종, 티켓: %s종\n<a href=\"%s\">%s</a>";
 			case "메가박스":
