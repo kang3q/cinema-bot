f0962310915caa34ca80597e4c9df2e9e70ec853
1004w455 <1004w455@naver.com>
2019-08-22 10:05:22 +0900

    doc: 알림 문구 수정

--- src/main/java/com/bot/cinemabot/model/MessageFormat.java
+++ src/main/java/com/bot/cinemabot/model/MessageFormat.java
@@ -22,18 +22,20 @@ public class MessageFormat {
 	}
 
 	private String getTextTemplate() {
+		// TODO 템플릿 파일로 분리를 하자
 		switch (this.platform) {
 			case "CGV":
-				return "%s\n%s\n오후 2시 판매시작!(or 4시)\n%s%s\n1+1영화:%s종%s\n구매링크:%s";
+				return "%s\n%s\n오후 2시 판매시작!(or 4시)\n%s%s\n1+1영화:%s종%s\n구매링크: %s";
 			case "롯데시네마":
-				return "%s\n%s\n%s\n%s원\n1+1영화:%s종, 티켓:%s종\n구매링크:%s";
+				return "%s\n%s\n%s\n%s원\n1+1영화:%s종, 티켓:%s종\n구매링크: %s";
 			case "메가박스":
-				return "%s\n%s\n%s\n%s원(개별사용 불가)\n1+1영화:%s종, 티켓:%s종\n구매링크:%s\n";
+				return "%s\n%s\n%s\n%s원(개별사용 불가)\n1+1영화:%s종, 티켓:%s종\n구매링크: %s";
 		}
 		throw new IllegalArgumentException(String.format("지원하지 않는 platform 입니다. %s", this.platform));
 	}
 
 	private String getHTMLTemplate() {
+		// TODO 템플릿 파일로 분리를 하자
 		switch (this.platform) {
 			case "CGV":
 				return "%s\n<b>%s</b>\n오후 2시 판매시작!(or 4시)\n%s%s\n1+1영화: %s종%s\n<a href=\"%s\">%s</a>";
@@ -46,3 +48,4 @@ public class MessageFormat {
 	}
 
 }
+// text 성공: 롯시, cgv
