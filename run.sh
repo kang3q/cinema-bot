f706914dc2dd58ca3c3fb8a3370175623c2d20cb
kang3q <kang3q@gmail.com>
2022-06-03 17:00:50 +0900

    chore: action 적용중(run.sh 경로 수정)

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
-sh stop.sh
+sh ~/cinema-bot/stop.sh
 
 echo 'start cinema-bot'
-nohup java -jar target/cinema-bot-0.0.1-SNAPSHOT.jar --spring.profiles.active=production --spring.bot.telegram.token=$CINEMA_BOT_TOKEN &
+nohup java -jar ~/cinema-bot/target/cinema-bot-0.0.1-SNAPSHOT.jar --spring.profiles.active=production --spring.bot.telegram.token=$CINEMA_BOT_TOKEN &
 
 echo 'END run.sh'
